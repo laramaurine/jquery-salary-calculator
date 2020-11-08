c38a72f993e0fa8d47447c04e589f46477396a15
Lara Lesar <lara.m.lesar@gmail.com>
2020-11-08 15:25:31 -0600

    errors in spelling fixed

--- client.js
+++ client.js
@@ -5,31 +5,24 @@ $( document ).ready( handleReady );
 function handleReady() {
     console.log('jQuery has joined the party');
     $('#submit').on('click', addEmployee);
-
+    $('#employeeTable').on('click', 'button.deleteBtn', deleteRow)
     //click handlers for adding and deleting will go here
 }
-
-//functions to gather form information
-//store the information  to calculate monthly costs
-//append information to dom
-//clear input fields(reset input val to empty string)
-//somewhere to store employees
 
 let employeeInventory = [];
 //employee info will be stored here
 
 function newEmployee(){
-    //will take data and push into array creating the new employee object
-    //console.log(first-name, last-name, emp-number, job-title, salary);
+    //will take data and push into array creating the new employee object 
+    console.log(firstName, lastName, empNumber, jobTitle, salary);
     const employee ={
-    
         firstName:firstName,
         lastName:lastName,
         employeeNumber:empNumber,
         jobTitle:jobTitle,
         salary:salary
-
     }
+    
     employeeInventory.push(employee)
     return true;
 }
@@ -44,19 +37,29 @@ function addEmployee(){
     console.log('adding employee');
 
     newEmployee(firstName, lastName, employeeNumber, jobTitle, salary);
+    appendEmployee();
+    console.log($('#firstName').val());
     //calling function to trigger push to array
+
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#empNumber').val('');
+    $('#jobTitle').val('');
+    $('#salary').val('');
+    //clears input fields
+
 }
 
 function appendEmployee(){
     console.log(employeeInventory);
-    $('#employee-table').empty();
-    for(let employee of employee){
+    $('#employeeTable').empty();
+    for(let employee of employeeInventory){
         $('#employee-table').append(
         `<tr> 
-            <td>${employee.first-name}</td>
-            <td>${employee.last-name}</td>
-            <td>${employee.emp-number}</td>
-            <td>${employee.job-title}</td>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.empNumber}</td>
+            <td>${employee.jobTitle}</td>
             <td>$${employee.salary}</td>
             <td><button class="deleteBtn">DELETE</button></td>
         </tr>`
@@ -77,6 +80,15 @@ function toggleClass(){
 }
 
 function deleteRow(){
+    console.log("clicked");
+    $(this).closest('tr').remove();
+
     //this function will delete an employee when clicked on the employee
     //in base mode it will not affect the total calculation
 }
+
+//functions to gather form information
+//store the information  to calculate monthly costs
+//append information to dom
+//clear input fields(reset input val to empty string)
+//somewhere to store employees
